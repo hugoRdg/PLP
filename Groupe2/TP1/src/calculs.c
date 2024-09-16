343777ab36876e5377c257f75d609eb3ff00c353
Hugo Rodrigues <hugo.rodrigues@iut-dijon.u-bourgogne.fr>
2024-09-16 11:26:30 +0200

    Gestion Exercice 1.8

---
+++ Groupe2/TP1/src/calculs.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int num1, num2;
+    char op;
+
+    // Demande d'entrer les valeurs
+    printf("Entrez le premier nombre : ");
+    scanf("%d", &num1);
+    printf("Entrez le second nombre : ");
+    scanf("%d", &num2);
+    printf("Entrez l'operateur (+, -, *, /, %, &, |, ~) : ");
+    scanf(" %c", &op);
+
+    switch (op) {
+        case '+':
+            printf("Resultat : %d\n", num1 + num2);
+            break;
+        case '-':
+            printf("Resultat : %d\n", num1 - num2);
+            break;
+        case '*':
+            printf("Resultat : %d\n", num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0) {
+                printf("Resultat : %d\n", num1 / num2);
+            } else {
+                printf("Erreur : Division par zero.\n");
+            }
+            break;
+        case '%':
+            if (num2 != 0) {
+                printf("Resultat : %d\n", num1 % num2);
+            } else {
+                printf("Erreur : Division par zero.\n");
+            }
+            break;
+        case '&':
+            printf("Resultat : %d\n", num1 & num2);
+            break;
+        case '|':
+            printf("Resultat : %d\n", num1 | num2);
+            break;
+        case '~':
+            printf("Resultat : %d\n", ~num1);
+            break;
+        default:
+            printf("Operateur non reconnu.\n");
+            break;
+    }
+
+    return 0;
+}
---
+++ Groupe2/TP1/src/calculs.py
@@ -0,0 +1,34 @@
+def main():
+    num1 = int(input("Entrez le premier nombre : "))
+    num2 = int(input("Entrez le second nombre : "))
+    op = input("Entrez l'opérateur (+, -, *, /, %, &, |, ~) : ")
+
+    match op:
+        case '+':
+            print(f"Résultat : {num1 + num2}")
+        case '-':
+            print(f"Résultat : {num1 - num2}")
+        case '*':
+            print(f"Résultat : {num1 * num2}")
+        case '/':
+            if num2 != 0:
+                print(f"Résultat : {num1 / num2}")
+            else:
+                print("Erreur : Division par zéro.")
+        case '%':
+            if num2 != 0:
+                print(f"Résultat : {num1 % num2}")
+            else:
+                print("Erreur : Division par zéro.")
+        case '&':
+            print(f"Résultat : {num1 & num2}")
+        case '|':
+            print(f"Résultat : {num1 | num2}")
+        case '~':
+            print(f"Résultat : {~num1}")
+        case _:
+            print("Opérateur non reconnu.")
+
+# Cette condition vérifie si le script est exécuté directement plutôt qu'importé comme module
+if __name__ == "__main__":
+    main()
